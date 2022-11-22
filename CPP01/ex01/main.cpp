9ff7a3a8be471a176ac3fafb7debe5fd969cc8b7
Elena LOUISIADE <elouisia@student.42.fr>
2022-11-22 14:46:35 +0100

    FIX HORDE ARG CHECKS:
    The type of input is now checked to prevent wrong input and overflows

--- CPP01/ex01/main.cpp
+++ CPP01/ex01/main.cpp
@@ -6,24 +6,59 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:34:04 by elouisia          #+#    #+#             */
-/*   Updated: 2022/11/22 12:29:38 by elouisia         ###   ########.fr       */
+/*   Updated: 2022/11/22 14:42:21 by elouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.h"
 #include <ctype.h>
 #include <stdlib.h>
+#include <cstring>
 
-int	main ( int ac, char **av ) {
+int ft_check_arg(int ac, char* av, long* n) {
+	
+	int i;
 	
 	if (ac != 3)
 	{
 		std::cerr << "Usage: ./horde nb_of_zombies name_of_zombies" << std::endl;
 		return 1;
 	}
-	Zombie* horde;
+	
+	i = strlen(av);
+	if (i > 11)
+	{
+		std::cerr << "Invalid type of argument" << std::endl;
+		return 1;
+	}
+	
+	for (int j = 0; j < i; j++)
+	{
+		if (!isdigit(av[j]))
+		{
+		std::cerr << "Invalid type of argument" << std::endl;
+		return 1;
+		}
+	}
+	
+	*n = atol(av);
+	if (*n < 1 || *n > 2147483647)
+	{
+		std::cerr << "Invalid type of argument" << std::endl;
+		return 1;
+	}
+	return 0;
+}
 
-	horde = zombieHorde(atoi(av[1]), av[2]);
+int	main ( int ac, char **av ) {
+	
+	long n;
+	Zombie* horde;
+	
+	if (ft_check_arg(ac, av[1], &n))
+		return 1;
+
+	horde = zombieHorde((int)n, av[2]);
 	if (horde == NULL)
 		return 1;
 	delete [] horde;
