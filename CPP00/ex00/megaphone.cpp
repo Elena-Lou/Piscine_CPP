801dde2a8ca7082a1b3d2c398f924fda67c6ae0f
Elena-Lou <elouisiade@live.fr>
2022-12-29 18:38:56 +0100

    ADD EX00 CPP00:
    The first exercise is completed but might need improvement to be
    less C-like.
    Awaiting Mich's review regarding spaces between strings

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/02 16:19:16 by elouisia          #+#    #+#             */
+/*   Updated: 2022/11/02 17:57:40 by elouisia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av) {
+	int	i;
+	int	j;
+
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)std::toupper(av[i][j]);
+			j++;
+		}
+		std::cout << " ";
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
