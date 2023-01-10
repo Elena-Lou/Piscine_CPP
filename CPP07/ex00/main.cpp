60edb6135d656535e53b949365199f8a0124de9e
Elena-Lou <elouisiade@live.fr>
2023-01-10 13:01:36 +0100

    UPADTE TESTS CPP06/EX00:

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 10:23:47 by elouisia          #+#    #+#             */
-/*   Updated: 2023/01/10 12:52:40 by elouisia         ###   ########.fr       */
+/*   Updated: 2023/01/10 13:01:24 by elouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int main( void ) {
 	
+	std::cout << "\n--- INT ---\n" ;
+
 	int a = 4;
 	int b = 3;
 	
@@ -22,7 +24,9 @@ int main( void ) {
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	
+
+	std::cout << "\n--- STRING ---\n" ;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	
@@ -31,5 +35,28 @@ int main( void ) {
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
+	std::cout << "\n--- FLOAT ---\n" ;
+	
+	float e = 42.42;
+	float f = 15.08;
+	
+	::swap( e, f );
+	
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
+	std::cout << "\n--- CHAR ---\n" ;
+
+	int g = 'A';
+	int h = 'z';
+	
+	::swap( g, h );
+	
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+	std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
+	
 	return 0;
 }
