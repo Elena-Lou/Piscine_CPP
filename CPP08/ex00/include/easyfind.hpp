b1dac1fec7b9f865fcf331119da3853c0cb80ffc
Elena-Lou <elouisiade@live.fr>
2023-01-12 16:21:57 +0100

    ADD CPP08/EX00:
    This is a combination of 2 commits.
     This is the 1st commit message:
    
    WIP CPP08/EX00 :
    
    easyFind function is up and running, now need to complete tests
    
     This is the commit message #2:
    
    WIP CPP08/EX00 :
    
    Tests with vectors / deque and list in main
    Invalid read when trying to display dereferenced iterator found

---
+++ CPP08/ex00/include/easyfind.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 10:24:04 by elouisia          #+#    #+#             */
+/*   Updated: 2023/01/12 16:38:21 by elouisia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <deque>
+#include <list>
+
+class ValueNotFound : public std::exception
+	{
+		public:
+		
+			const char* what( void ) const throw() {
+				return ("Value not found");
+			};
+	};
+	
+template <typename T>
+typename T::iterator easyfind ( T container, int toBeFound ) {
+
+	typename T::iterator it;
+	it = find(container.begin(), container.end(), toBeFound);
+	
+	if (it != container.end())
+	{
+		std::cout << "Found it " << std::endl;
+		return (it);
+	}
+	else
+		throw ValueNotFound();
+	
+}
+
+
+
+#endif
---
+++ CPP08/ex00/main.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/12 10:23:48 by elouisia          #+#    #+#             */
+/*   Updated: 2023/01/12 16:45:55 by elouisia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main ( void ) {
+	
+	std::cout << "\n--- VECTOR ---\n" << std::endl;
+	
+	try
+	{
+		std::vector<int> firstVector;
+		for (int i = 0; i < 200; i++)
+		{
+			firstVector.push_back(i);
+		}
+	
+		std::vector<int>::iterator found;
+	
+		found = easyfind(firstVector, 42);
+		// std::cout << *found << std::endl;
+	}
+	catch ( std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::cout << "\n--- DEQUE ---\n" << std::endl;
+	
+	try
+	{
+		std::deque<int> firstDeque;
+		for (int i = 0; i < 200; i++)
+		{
+			firstDeque.push_back(i);
+		}
+	
+		std::deque<int>::iterator found;
+	
+		found = easyfind(firstDeque, 200);
+		// std::cout << *found << std::endl;
+	}
+	catch ( std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	
+	std::cout << "\n--- LIST ---\n" << std::endl;
+	try
+	{
+		std::list<int> firstList;
+		for (int i = 0; i < 200; i++)
+		{
+			firstList.push_back(i);
+		}
+	
+		std::list<int>::iterator found;
+	
+		found = easyfind(firstList, 45);
+		// std::cout << *found << std::endl;
+	}
+	catch ( std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+}
