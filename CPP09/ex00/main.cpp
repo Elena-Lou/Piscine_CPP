9cf7c500a9ace913e1755dfa9db906f5dab0501e
Elena-Lou <elouisiade@live.fr>
2023-04-12 13:19:55 +0200

    WIP EX00/CPP09:
    Test to open and read from input.txt

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,8 +1,28 @@
 #include "./include/BitcoinExchange.hpp"
 
-int main ( void ) {
+int main ( int ac, char **av ) {
 
-	std::cout << "BOUH" << std::endl;
+	if (ac != 2)
+	{
+		std::cerr << "Invalid number of arguments" << std::endl;
+		return 1;
+	}
+
+	std::ifstream	input;
+	std::string 	line;
+
+	input.open(av[1], std::ifstream::in);
+	if (!input)
+	{
+		std::cerr << "Open error" << std::endl;
+		return 1;
+	}
+	while (getline(input, line))
+	{
+		std::cout << line << std::endl;
+	}
+
+	std::cout << "\n --- test date --- \n" << std::endl;
 
 	const char date1[] = "2011-01-03";
 	const char date2[] = "2001-42-42";
@@ -13,6 +33,6 @@ int main ( void ) {
 	if (!strptime(date2, "%Y-%m-%d", &tm))
 		std::cout << "not valid date" << std::endl;
 
-
+	input.close();
 	return 0;
 }
